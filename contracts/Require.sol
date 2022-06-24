b3e7d11c7ab9928b22e84cb71d690d8e7843e1b3
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-06-24 19:50:47 +0530

    Update Require.sol

--- contracts/Require.sol
+++ contracts/Require.sol
@@ -3,8 +3,5 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Require {
-    function checkInput(uint256 _input) public pure returns (string memory) {
-        require(_input >= 0 && _input <= 255, "Not Within Range");
-        return "Within Range";
-    }
+    
 }
